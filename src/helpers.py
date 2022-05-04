77e022b35580a7bbd9634a25f0071e1870ee2be3
Pierre Epron <pierre.epron@gmail.com>
2022-05-04 20:09:57 +0200

    add load_rasa_data helper

--- src/helpers.py
+++ src/helpers.py
@@ -16,3 +16,24 @@ def convert_path(path : Any) -> Path:
             converted path.
     """
     return path if isinstance(path, Path) else Path(path)
+
+def load_rasa_data(path : Any, encoding : Text = 'utf-8'):
+    """
+        Shortcut for load rasa train data with RasaYAMLReader.
+        
+        Parameters
+        ----------
+        path : str or pathlib.Path
+            path to convert.
+        encoding : str, default 'utf-8'
+            encoding using when read file
+        
+        Return
+        ------
+        pathlib.Path
+            converted path.
+    """
+    # Convert part if necessary
+    path = convert_path(path)
+    # Read and parse rasa data
+    return RasaYAMLReader().reads(path.read_text(encoding=encoding))
